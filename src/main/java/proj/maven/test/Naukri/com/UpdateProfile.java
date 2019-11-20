a3d3730036c25eca3ea2b04f0fd623280385a3f1
dtf8207 <dtf8207@PU8L891FNN2.vms.ad.varian.com>
2019-11-20 16:39:31 +0530

    Updated unit test

--- src/main/java/proj/maven/test/Naukri/com/UpdateProfile.java
+++ src/main/java/proj/maven/test/Naukri/com/UpdateProfile.java
@@ -1,22 +1,35 @@
 package proj.maven.test.Naukri.com;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
+
 import org.openqa.selenium.interactions.Actions;
+
+import com.sun.org.apache.xpath.internal.operations.Equals;
 
 public class UpdateProfile 
 {
-    public static void main( String[] args ) throws InterruptedException
+    public static void main( String[] args ) throws InterruptedException, IOException
     {
-     
     	
+    	
+     FileInputStream fis = new FileInputStream("config.properties");
+    Properties prop = new Properties();	
+	prop.load(fis);
     	System.setProperty("webdriver.chrome.driver",
-				System.getProperty("user.dir")+"\\resourses\\chromedriver.exe");
+				System.getProperty("user.dir")+"\\resource_new\\chromedriver.exe");
+    	
+    
+    	
     	
     	
 	WebDriver driver = new ChromeDriver();
@@ -27,9 +40,21 @@ public class UpdateProfile
 
 		
 		driver.get("https://www.naukri.com/");
+		
+String MainWindow=driver.getWindowHandle();
+		
+		Set<String> handle = driver.getWindowHandles();
+		
+		System.out.println(MainWindow);
+		for(String a : handle ) {
+			if(!a.equals(MainWindow)) {
+				driver.switchTo().window(a).close();
+			}
+		}
+		driver.switchTo().window(MainWindow);
 		driver.findElement(By.xpath("//*[@id='login_Layer']/div")).click();
-		driver.findElement(By.xpath("//*[@id='eLoginNew']")).sendKeys("sanketpandey2@gmail.com");
-		driver.findElement(By.xpath("//*[@id='pLogin']")).sendKeys("India@123");
+		driver.findElement(By.xpath("//*[@id='eLoginNew']")).sendKeys(prop.getProperty("signInEmail_id"));
+		driver.findElement(By.xpath("//*[@id='pLogin']")).sendKeys(prop.getProperty("signInPWD_id"));
 		driver.findElement(By.xpath("//*[@id='lgnFrmNew']/div[9]/button")).click();
 		 WebElement elm = driver.findElement(By.xpath("/html/body/div[1]/div/div/ul[2]/li[2]/a/div[2]"));
 		 
@@ -41,7 +66,9 @@ public class UpdateProfile
 		
 		driver.findElement(By.xpath("/html/body/div[5]/div[8]/div[2]/form/div[3]/div/button")).submit();
 		
-		Thread.sleep(1000);
+		
+		
+		Thread.sleep(10000);
 		
 		driver.close();
 		
